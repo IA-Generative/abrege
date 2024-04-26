f5df56d0b0e075437c5d3f6f0bd8ad4837bd2c89
Alexandre Bigot <alexandre-hadrien.bigot@interieur.gouv.fr>
2024-04-26 16:56:13 +0200

    add fastapi

--- app/main.py
+++ app/main.py
@@ -2,21 +2,149 @@ from fastapi import FastAPI
 import os, sys, logging, random, asyncio, uvicorn, time
 from pathlib import Path
 
+from typing import Union
+import logging
+
+import os
+import sys
+import json
+import time
+import re
+from contextlib import asynccontextmanager
+from pathlib import Path
+from datetime import datetime
+from collections import Counter
+from typing import Dict, List
+
+from fastapi import FastAPI, HTTPException, status, Security, UploadFile
+from fastapi.security.api_key import APIKeyHeader
+from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_openai import ChatOpenAI
+
+from fastapi import FastAPI
+
+
+origins = [
+    "https://sie.numerique-interieur.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+logger = logging.getLogger(__name__)
+context = {}
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """
+    Load the resources used by the API (models, data)
+    """
+    import nltk
+
+    nltk.download("punkt")
+
+    from langchain_community.embeddings import HuggingFaceEmbeddings
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="OrdalieTech/Solon-embeddings-base-0.1"
+    )  # plus de 13 min
+    logger.info(f"Embedding model {repr(embeddings)} available")
+    import sys
+
+    sys.path.append("./src")
+    from summary_chain import summarize_chain_builder, EmbeddingModel
+
+    embedding_model = EmbeddingModel(embeddings, "HuggingFaceEmbeddings")
+
+    import os
+
+    OPENAI_API_BASE = os.environ.get["OPENAI_API_BASE"]
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+    from langchain_openai import ChatOpenAI
+
+    llm = ChatOpenAI(
+        api_key=OPENAI_API_KEY,
+        openai_api_base=OPENAI_API_BASE,
+        temperature=0,
+        model="mixtral",
+    )
+
+    custom_chain = summarize_chain_builder(
+        method="k-means", embedding_model=embedding_model, llm=llm
+    )
+
+    context["chain"] = custom_chain
+
+    logger.info("======== Lifespan initialization done =========")
+
+    yield
+    # Clean up the resources
+    context.clear()
+
+
 logger = logging.getLogger()
 
-with Path('./README.md').open() as f: description = f.readline()
+description = Path("./README.md").read_text()
+
+
+API_KEY_HEADER = APIKeyHeader(name="x-api-key", auto_error=False)
 
 app = FastAPI(
-    title="abrege",
-    description=description,
-    version="0.0.1",
+    title="abrege", description=description, version="0.0.1", lifespan=lifespan
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", status_code=200)
+async def healthcheck():
+    return "Hello"
 
 
 @app.get("/healthcheck", status_code=200)
 async def healthcheck():
     return
 
+
+@app.get("/camus")
+def read_item(url: str):
+    text2 = Path("./data/camus.txt").read_text()
+    res = context["chain"].invoke(text2)
+    return res
+
+
+@app.get("/url/{url}")
+def read_item(url: str):
+    from urllib.parse import urlparse, urlsplit
+
+    parsed_url = urlparse(url)
+    if not (parsed_url.scheme and parsed_url.netloc):
+        url = "https://www.jesuismort.com/tombe/albert-camus"
+
+    if 1:
+        from langchain_community.document_loaders import UnstructuredURLLoader
+
+        loader = UnstructuredURLLoader(urls=[url])
+    else:
+        from langchain_community.document_loaders import SeleniumURLLoader
+
+        loader = SeleniumURLLoader(urls=[url])
+    data: list = loader.load()
+
+    res = [context["chain"].invoke(doc.page_content) for doc in data]
+
+    return "\n\n".join(res)
 
 
 if __name__ == "__main__":
