7d35e1a88779cb8870820d2b4e69607b3831c544
fura <faucheux_jerome@orange.fr>
2024-05-17 14:14:38 +0200

    Front avec connexion à l'api

--- front/main.py
+++ front/main.py
@@ -12,22 +12,20 @@ stdsfr.override_font_family()
 
 @st.cache_data
 def get_param():
-    try:
-        response = requests.get(base_api_url + "/default_params")
-        if response.status_code == 200:
-            payload = json.loads(response.content)
-            return payload
-        else:
-            return None
-    except requests.exceptions.ConnectionError:
+    response = requests.get(base_api_url + "/default_params")
+    if response.status_code == 200:
+        payload = json.loads(response.content)
+        return payload
+    else:
         return None
 
 
-available_params = get_param()
-
-if available_params is None:
+try:
+    available_params = get_param()
+except requests.exceptions.ConnectionError:
     stdsfr.alert("Erreur lors du chargement de la configuration initiale", type="error")
     st.stop()
+
 
 params = {}
 
@@ -67,3 +65,70 @@ params["question_template"] = expander3.text_area(
 params["refine_template"] = expander3.text_area(
     label="refine_template", value=available_params["prompt_template"]["refine"]
 )
+
+st.header("Résumeur de documents")
+
+st.write(
+    """Cette page web propose un service pour résumer un texte, une page web via une 
+url ou bien un document. Le résumé est effectué à l'aide d'un LLM du MIOM, souverain et 
+sans collecte de vos données. Les résumés produits peuvent être parametrisés à l'aide 
+du menu déroulant à gauche"""
+)
+
+doc_type = st.selectbox(
+    label="Choisissez un type de documents à résumer",
+    options=["texte", "URL", "document"],
+    index=2,
+)
+
+if doc_type == "texte":
+    user_input = st.text_area(label="Entrer votre texte", placeholder="Texte à résumer")
+elif doc_type == "URL":
+    user_input = st.text_area(
+        label="Entrer votre URL", placeholder="URL vers la page web à résumer"
+    )
+elif doc_type == "document":
+    stdsfr.alert(
+        "Attention: les documents scannés ne sont pas supportés pour le moment"
+    )
+    user_input = stdsfr.dsfr_file_uploader(
+        label="Téléverser votre document", help="Documents acceptés: .pdf, .docx, .odt"
+    )
+
+
+def ask_llm(request_type, params, user_input) -> str:
+    with st.spinner("Résumé en cours de fabrication..."):
+        if request_type == "texte":
+            url = base_api_url + "/text"
+            params |= {"text": user_input}
+            response = requests.get(url, params=params)
+        elif request_type == "URL":
+            url = base_api_url + "/url"
+            params |= {"url": user_input}
+            response = requests.get(url, params=params)
+        elif request_type == "document":
+            url = base_api_url + "/doc"
+            file = {"file": (user_input.name, user_input)}
+            response = requests.post(url=url, files=file, params=params)
+
+    if response.status_code == 200:
+        summary = json.loads(response.content)["summary"]
+        return summary
+    else:
+        if response.status_code == 500:
+            stdsfr.alert("Erreur interne au service", type="error")
+        elif response.status_code == 400:
+            stdsfr.alert(
+                "Impossible de traiter les documents. Veuillez vérifier que l'url est bonne ou que les documents ne sont pas scannés."  # noqa
+            )
+
+
+# stdsfr button not working, seems to rerun to early to let st.spinner work
+# maybe should open a pull request
+if st.button("Générer un résumé"):
+    summary = ask_llm(doc_type, params, user_input)
+    st.session_state.summary = summary
+
+if "summary" in st.session_state:
+    st.subheader("Résumé:")
+    st.write(st.session_state.summary)
