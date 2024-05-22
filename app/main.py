3d08cd3afc897b488eeea42cb401ac3ce962d852
Alexandre Bigot <alexandre-hadrien.bigot@interieur.gouv.fr>
2024-05-22 14:47:35 +0200

    add env for test in gh

--- app/main.py
+++ app/main.py
@@ -101,10 +101,10 @@ async def lifespan(_: FastAPI):
 
             context["chat_builder"] = chat_builder
 
-            embedding_model = OpenAIEmbeddingFunction(
+            openai_embedding_function = OpenAIEmbeddingFunction(
                 api_key=OPENAI_EMBEDDDING_KEY, api_base=OPENAI_EMBEDDING_BASE
             )
-            embedding_model = EmbeddingModel(embedding_model)
+            embedding_model = EmbeddingModel(openai_embedding_function)
             context["embedding_model"] = embedding_model
         else:
 
