8877d4132b88249fac2a0089280464e9ae4f9e1e
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-26 22:18:58 +0200

    fix: add client in right domain

--- api/schemas/response.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class SummaryResponse(BaseModel):
-    summary: str
-    nb_call: int | None = None
-    time: float | None = None
-    nb_words: int | None = 0
--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,12 +1,7 @@
-from .marker_client import MarkerAPIClient
-from ..config.marker_api import MarkerAPISettings
 from src.config.celery import CelerySettings
 from src.config.redis import RedisSettings
 from celery import Celery
 
-marker_api_setting = MarkerAPISettings()
-
-client_marker = MarkerAPIClient(base_url=marker_api_setting.MARKER_API_BASE_URL)
 
 celery_config = CelerySettings()
 redis_settings = RedisSettings()
