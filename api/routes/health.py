1f126e0d989ce7854687bf40985b405d0a2aaef8
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-26 21:57:24 +0200

    feat: add s3 and minio connector

--- api/routes/health.py
+++ api/routes/health.py
@@ -17,22 +17,30 @@ up_time = datetime.datetime.now().isoformat()
 
 def get_paddle_ocr_health() -> Union[Health, HealthError]:
     service_name = "PADDLE_OCR"
-    res = requests.get(
-        url=settings.PADDLE_OCR_URL,
-    )
-    if res.status_code == 200:
-        logger_abrege.debug("Check Paddle")
-        health = Health(
-            name=service_name,
-            version="",
-            up_time=up_time,
+    try:
+        res = requests.get(
+            url=settings.PADDLE_OCR_URL,
         )
-        return health
-    else:
-        logger_abrege.error("Paddle not available")
-        error = HealthError(name=service_name, error="", code_status=500)
+        if res.status_code == 200:
+            logger_abrege.debug("Check Paddle")
+            health = Health(
+                name=service_name,
+                version="",
+                up_time=up_time,
+            )
+            return health
+        else:
+            logger_abrege.error("Paddle not available")
+            error = HealthError(
+                name=service_name,
+                error="Paddle not available",
+                code_status=res.status_code,
+            )
 
-        return error
+            return error
+    except Exception as e:
+        logger_abrege.error("Paddle not available")
+        return HealthError(name=service_name, error=f"{str(e)}", code_status=500)
 
 
 @router.get("/", status_code=200, response_model=Health)
@@ -42,6 +50,13 @@ async def healthcheck():
     health_paddle_api = get_paddle_ocr_health()
     if not isinstance(health_paddle_api, Health):
         status = "unhealthy"
-
+    dependencies.append(health_paddle_api)
     logger_abrege.debug("Health avalaible")
-    return Health(name=__name__, version=__version__, up_time=up_time, extras={}, status=status, dependencies=dependencies)
+    return Health(
+        name=__name__,
+        version=__version__,
+        up_time=up_time,
+        extras={},
+        status=status,
+        dependencies=dependencies,
+    )
