44f5823b81ebc61457d375d2a3d9ac5037376188
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-27 13:12:49 +0200

    feat: add audio service to text

---
+++ abrege_service/modules/audio.py
@@ -0,0 +1,114 @@
+from abc import abstractmethod
+import json
+
+from vosk import Model, KaldiRecognizer
+from pydub import AudioSegment
+import wave
+
+from abrege_service.schemas import AUDIO_CONTENT_TYPES
+from abrege_service.modules.base import BaseService
+from pydantic import BaseModel
+from typing import List
+
+# audio dataset : https://github.com/facebookresearch/voxpopuli
+# https://lbourdois.github.io/blog/audio/dataset_audio_fr/
+# SAMPLE https://github.com/UniData-pro/french-speech-recognition-dataset/tree/main
+
+
+class Words(BaseModel):
+    """
+    Model for words in audio processing.
+    """
+
+    word: str
+    start: float
+    end: float
+    conf: float
+
+
+class AudioModel(BaseModel):
+    """
+    Model for audio processing.
+    """
+
+    result: List[Words]
+    text: str
+
+
+def convertir_audio(chemin_entree, chemin_sortie):
+    # Charger le fichier audio
+    audio = AudioSegment.from_file(chemin_entree)
+
+    # Convertir en mono et définir le taux d'échantillonnage à 16 kHz
+    audio = audio.set_channels(1).set_frame_rate(16000).set_sample_width(2)
+
+    # Exporter en WAV avec une profondeur de bits de 16 bits
+    audio.export(chemin_sortie, format="wav", bitrate="16k")
+
+
+class AudioBaseService(BaseService):
+    def is_availble(self, content_type: str) -> bool:
+        """
+        Check if the content type is available for processing.
+
+        Args:
+            content_type (str): The content type to check.
+
+        Returns:
+            bool: True if the content type is available for processing, False otherwise.
+        """
+        return content_type in AUDIO_CONTENT_TYPES
+
+    @abstractmethod
+    def audio_to_text(self, file_path: str, **kwargs) -> str: ...
+
+
+class AudioService(AudioBaseService):
+    def __init__(self, path_model: str = "abrege_service/data/models/vosk-model-small-fr-0.22"):
+        self.model = Model(path_model)
+        self.rec = KaldiRecognizer(self.model, 16000)
+        self.rec.SetWords(True)
+
+    def is_availble(self, content_type: str) -> bool:
+        """
+        Check if the content type is available for processing.
+
+        Args:
+            content_type (str): The content type to check.
+
+        Returns:
+            bool: True if the content type is available for processing, False otherwise.
+        """
+        return content_type in AUDIO_CONTENT_TYPES
+
+    def audio_to_text(self, file_path: str, **kwargs) -> List[AudioModel]:
+        convertir_audio(file_path, "temp.wav")
+        file_path = "temp.wav"
+        wf = wave.open(file_path, "rb")
+        print(f"Audio file: {file_path} - Channels: {wf.getnchannels()} - Sample Width: {wf.getsampwidth()} - Frame Rate: {wf.getframerate()}")
+
+        if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getframerate() != 16000:
+            raise Exception("Audio file must be WAV format mono PCM.")
+
+        rec = KaldiRecognizer(self.model, wf.getframerate())
+        rec.SetWords(True)
+        results = []
+        while True:
+            data = wf.readframes(4000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                res = json.loads(rec.Result())
+                results.append(AudioModel.model_validate(res))
+
+        final_res = json.loads(rec.FinalResult())
+        results.append(AudioModel.model_validate(final_res))
+        wf.close()
+
+        return results
+
+    def transform_to_text(self, file_path, content_type, **kwargs) -> str:
+        if not self.is_availble(content_type):
+            raise NotImplementedError(f"Content type {content_type} is not implemented for {self.__class__.__name__}.")
+
+        return "\n".join([audio.text for audio in self.audio_to_text(file_path, **kwargs)])
---
+++ tests/abrege_service/modules/test_audio_module.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+from abrege_service.modules.audio import AudioService, AudioModel
+
+
+@pytest.mark.skipif(
+    os.parth.exists("abrege_service/data/models/vosk-model-small-fr-0.22") is False,
+    reason="Model not found",
+)
+def test_audio_service():
+    # wget https://alphacephei.com/vosk/models/vosk-model-small-fr-0.22.zip
+    # unzip vosk-model-small-fr-0.22.zip -d abrege_service/data/models
+    audio_service = AudioService("abrege_service/data/models/vosk-model-small-fr-0.22")
+    assert audio_service.is_availble("audio/mpeg") is True
+    assert audio_service.is_availble("application/pdf") is False
+    for audio in audio_service.audio_to_text("tests/data/audio/1.wav"):
+        assert isinstance(audio, AudioModel)
+
+    assert isinstance(
+        audio_service.transform_to_text("tests/data/audio/1.wav", content_type="audio/mpeg"),
+        str,
+    )
