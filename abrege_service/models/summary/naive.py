73d54e6f0a087b98aab86dc9c1fafe4b952aca98
michou <misu.helal-ali@interieur.gouv.fr>
2025-04-28 23:32:24 +0200

    feat: add naive approach

--- abrege_service/models/summary/naive.py
+++ abrege_service/models/summary/naive.py
@@ -1,11 +1,12 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from openai import OpenAI
 import time
+import math
 
 from api.utils.logger import logger_abrege as logger_app
-from abrege_service.utils.text import split_texts_by_word_limit
 from abrege_service.prompts.prompting import generate_prompt
-
+from src.schemas.result import SummaryModel
+from src.schemas.task import TaskModel, task_table, TaskStatus, TaskUpdateForm
 
 CONTEXT_LENGTH = 128_000  # For qwen
 
@@ -31,68 +32,93 @@ def summarize_text(model: str, client: OpenAI, prompt: str, temperature: float =
 
 
 def merge_summaries(
+    task: TaskModel,
     summaries: List[str],
     model: str,
     client: OpenAI,
     size: Optional[int] = 300,
     language: Optional[str] = None,
-) -> str:
+) -> Tuple[TaskModel, int]:
     """
     Combine récursivement les résumés en un seul.
     """
+    # Theoretically, the number of calls to the LLM is log2(n) + 1
     nb_call = 0
+    total_call = int(math.log(len(summaries), 2)) + 1
+    partial_summary: SummaryModel = task.result
+    if partial_summary is None:
+        partial_summary = SummaryModel(
+            created_at=int(time.time()),
+            updated_at=int(time.time()),
+            summary="",
+            word_count=0,
+            percentage=0,
+            model_name=model,
+            model_version=model,
+            status=TaskStatus.IN_PROGRESS.value,
+            extras={"previous_summary": []},
+        )
+
     while len(summaries) > 1:
         new_summaries = []
         for i in range(0, len(summaries), 2):
             if i + 1 < len(summaries):
+                summary1 = summaries[i]
+                summary2 = summaries[i + 1]
+                previous_summaries = {
+                    "sumaries_id": [summary1, summary2],
+                    "summaries": [summary1, summary2],
+                    "word_count": [len(summary1.split()), len(summary2.split())],
+                    "model_name": model,
+                    "step": nb_call,
+                }
+
                 prompt = generate_prompt(
                     template_name="segement_summary_promt.jinja2",
                     context={
                         "size": size,
                         "language": language,
-                        "summaries": [summaries[i], summaries[i + 1]],
+                        "summaries": [summary1, summary2],
                     },
                 )
+                t = time.time()
                 new_summary = summarize_text(model, client, prompt)
+                time_merge = time.time() - t
+                previous_summaries["time"] = time_merge
+                partial_summary.extras["previous_summary"].append(previous_summaries)
                 new_summaries.append(new_summary)
                 nb_call += 1
+
+                partial_summary.updated_at = int(time.time())
+                partial_summary.percentage = nb_call / total_call
+                partial_summary.word_count = len(new_summary.split())
+                partial_summary.summary = new_summary
+                task = task_table.update_task(
+                    task_id=task.id,
+                    form_data=TaskUpdateForm(
+                        status=TaskStatus.IN_PROGRESS.value,
+                        result=partial_summary,
+                        updated_at=int(time.time()),
+                    ),
+                )
+                logger_app.debug(f"New summary: {new_summary} - Time: {time_merge} - Call: {nb_call}")
+                print(task.result)
+                print(79 * "+")
             else:
                 new_summaries.append(summaries[i])
         summaries = new_summaries
-    return summaries[0], nb_call
 
-
-def process_documents(
-    docs: List[str],
-    model: str,
-    client: OpenAI,
-    size: Optional[int] = 300,
-    language: Optional[str] = None,
-) -> str:
-    """
-    Traite une liste de documents pour produire un résumé final.
-    """
-    t = time.time()
-    partial_summaries = []
-
-    docs = split_texts_by_word_limit(docs, max_words=CONTEXT_LENGTH)
-    nb_words = 0
-    for doc in docs:
-        nb_curr_words = len(doc.split())
-        nb_words += nb_curr_words
-        logger_app.debug(f"Current words: {nb_curr_words} - {nb_words}")
-        prompt = generate_prompt(
-            template_name="segement_summary_promt.jinja2",
-            context={
-                "size": size,
-                "language": language,
-                "summaries": [doc],
-            },
-        )
-        partial_summary = summarize_text(model, client, prompt=prompt)
-        partial_summaries.append(partial_summary)
-    logger_app.debug(f"Total words {nb_words}")
-    logger_app.debug(f"Partial summaries: {len(partial_summaries)} - {time.time() - t}")
-    nb_call_llm = len(partial_summaries)
-    final_summary, nb_call_llm_merge = merge_summaries(partial_summaries, model, client)
-    return {"summary": final_summary, "nb_call": nb_call_llm + nb_call_llm_merge}
+    final_summary = summaries[0]
+    partial_summary.updated_at = int(time.time())
+    partial_summary.percentage = 1
+    partial_summary.word_count = len(final_summary.split())
+    partial_summary.summary = final_summary
+    task = task_table.update_task(
+        task_id=task.id,
+        form_data=TaskUpdateForm(
+            status=TaskStatus.COMPLETED.value,
+            result=partial_summary,
+            updated_at=int(time.time()),
+        ),
+    )
+    return task, nb_call
--- tests/abrege_service/models/summary/test_naive_process.py
+++ tests/abrege_service/models/summary/test_naive_process.py
@@ -1,20 +1,27 @@
 import os
 import pytest
-
-
-@pytest.mark.skipif(
-    not os.environ.get("OPENAI_API_KEY", None) or not os.environ.get("OPENAI_API_URL", None) or not os.environ.get("OPENAI_MODEL_NAME", None),
-    reason="API key and url not set",
+import math
+from abrege_service.models.summary.naive import (
+    summarize_text,
+    merge_summaries,
 )
+from abrege_service.prompts.prompting import generate_prompt
+from src.schemas.task import task_table, TaskStatus, TaskForm
+import openai
+from datasets import load_dataset
+
+is_openai_available = (
+    not os.environ.get("OPENAI_API_KEY", None) or not os.environ.get("OPENAI_API_BASE", None) or not os.environ.get("OPENAI_API_MODEL", None)
+)
+
+message = "API key and url not set"
+
+
+@pytest.mark.skipif(is_openai_available, reason=message)
 def test_prompt_summary_process():
     """
     Test the naive process of loading a dataset in streaming mode.
     """
-    from datasets import load_dataset
-    from evaluate import load
-    from abrege_service.models.summary.naive import summarize_text
-    from abrege_service.prompts.prompting import generate_prompt
-    import openai
 
     # Chargement en streaming du sous-ensemble français
     dataset_stream = load_dataset(
@@ -24,35 +31,90 @@ def test_prompt_summary_process():
         streaming=True,
     )
 
-    rouge_metric = load("rouge")
-
     # Load the first batch of data
     client = openai.OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY"),
-        base_url=os.environ.get("OPENAI_API_URL"),
+        base_url=os.environ.get("OPENAI_API_BASE"),
     )
-    dataset = dataset_stream["train"].shuffle(seed=42).take(10)
+    dataset = dataset_stream["train"].shuffle(seed=42).take(2)
 
     predictions = []
-    gt_summaries = []
     for data in dataset:
         assert "text" in data
         assert "summary" in data
         pred = summarize_text(
-            model=os.environ.get("OPENAI_MODEL_NAME"),
+            model=os.environ.get("OPENAI_API_MODEL"),
             client=client,
             prompt=generate_prompt(
                 "segement_summary_promt.jinja2",
                 {"size": 100, "language": "français", "text": data["text"]},
             ),
         )
+        assert isinstance(pred, str)
         predictions.append(pred)
-        gt_summaries.append(data["summary"])
 
-    # Calculate the Rouge score
-    rouge_score = rouge_metric.compute(
-        predictions=predictions,
-        references=gt_summaries,
-        use_stemmer=True,
+    assert len(predictions) == 2
+
+
+@pytest.mark.skipif(is_openai_available, reason=message)
+def test_merge_summary_small_text():
+    """
+    Test the naive process of loading a dataset in streaming mode.
+    """
+
+    # Load the first batch of data
+    client = openai.OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        base_url=os.environ.get("OPENAI_API_BASE"),
     )
-    print(f"Rouge score: {rouge_score}")
+    task = task_table.insert_new_task(
+        user_id="test",
+        form_data=TaskForm(type="summary", status=TaskStatus.CREATED.value),
+    )
+
+    task, nb_call = merge_summaries(
+        task=task,
+        summaries=["test", "test2"],
+        model=os.environ.get("OPENAI_API_MODEL"),
+        client=client,
+    )
+    assert task.status == TaskStatus.COMPLETED.value
+    assert nb_call == 1
+    task_table.delete_task_by_id(task.id)
+
+
+@pytest.mark.skipif(True, reason=message)
+def test_merge_summary_large_text():
+    dataset_stream = load_dataset(
+        "csebuetnlp/xlsum",
+        "french",
+        cache_dir="tests/data/text",
+        streaming=True,
+    )
+    client = openai.OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        base_url=os.environ.get("OPENAI_API_BASE"),
+    )
+
+    task = task_table.insert_new_task(
+        user_id="test",
+        form_data=TaskForm(type="summary", status=TaskStatus.CREATED.value),
+    )
+    n = 3
+    dataset = dataset_stream["train"].shuffle(seed=42).take(n)
+
+    texts = []
+    for data in dataset:
+        texts.append(data["text"])
+
+    # Here work with gemma3
+    task, nb_call = merge_summaries(
+        task=task,
+        summaries=texts,
+        model=os.environ.get("OPENAI_API_MODEL"),
+        client=client,
+    )
+    assert task.status == TaskStatus.COMPLETED.value
+    assert nb_call == int(math.log(n, 2) + 1)
+    assert task.result.percentage == 1
+    task_table.delete_task_by_id(task.id)
