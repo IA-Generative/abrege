f5d1ea11ed5e26f500a5085061c163ce7f66a77d
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-01 00:20:23 +0200

    feat: add ocr purpose

--- abrege_service/modules/image.py
+++ abrege_service/modules/image.py
@@ -3,23 +3,5 @@ from abrege_service.modules.base import BaseService
 
 
 class ImageService(BaseService):
-    def is_availble(self, content_type: str) -> bool:
-        """
-        Check if the content type is available for processing.
-
-        Args:
-            content_type (str): The content type to check.
-
-        Returns:
-            bool: True if the content type is available for processing, False otherwise.
-        """
-        return content_type in IMAGE_CONTENT_TYPES
-
-    def image_to_text(self, file_path: str, **kwargs) -> str:
-        raise NotImplementedError("Image to text conversion is not implemented.")
-
-    def transform_to_text(self, file_path, content_type, **kwargs) -> str:
-        if not self.is_availble(content_type):
-            raise NotImplementedError(f"Content type {content_type} is not implemented for {self.__class__.__name__}.")
-
-        return self.image_to_text(file_path, **kwargs)
+    def __init__(self, content_type_allowed=IMAGE_CONTENT_TYPES):
+        super().__init__(content_type_allowed)
--- src/schemas/result.py
+++ src/schemas/result.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Any
+from typing import Dict, Optional, Any, List
 from pydantic import BaseModel, ConfigDict
 
 
@@ -9,6 +9,7 @@ class ResultModel(BaseModel):
     model_name: str
     model_version: str
     updated_at: Optional[int] = None
+    texts_found: Optional[List[str]] = []
     percentage: float = 0.0
     extras: Optional[Dict[str, Any]] = None
 
