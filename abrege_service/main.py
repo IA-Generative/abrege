ad4ca7a7d0c8d38651132f40a1ed0b0b8f2b5928
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-01 09:42:49 +0200

    feat: use services approach

--- abrege_service/main.py
+++ abrege_service/main.py
@@ -1,32 +1,45 @@
 import os
+from typing import List
 import time
 import json
 
 import openai
 
-from abrege_service.modules.audio import AudioService
-from abrege_service.modules.doc import DocService
-from abrege_service.modules.video import VideoService
+from abrege_service.modules.base import BaseService
 from abrege_service.modules.url import URLService
-from abrege_service.schemas.text import TextModel as TextModelService
+from abrege_service.modules.audio import AudioVoskTranscriptionService
+from abrege_service.modules.video import VideoTranscriptionService
+from abrege_service.modules.doc import (
+    MicrosoftDocumnentToMdService,
+    FlatTextService,
+    PDFTOMD4LLMService,
+)
+
 from abrege_service.models.summary.naive import merge_summaries
 from abrege_service.utils.text import split_texts_by_word_limit
 from abrege_service.config.openai import OpenAISettings
 
 from src.schemas.task import TaskModel, task_table, TaskStatus, TaskUpdateForm
 from src.schemas.content import URLModel, DocumentModel, TextModel
+from src.schemas.result import ResultModel
 from src.clients import celery_app, file_connector
+from src import __version__
 
 
-audio_service = AudioService()
-video_service = VideoService(audio_service=audio_service)
-document_service = DocService(audio_service=audio_service, video_service=video_service)
 # TODO: transform to url service to use only doc service for document process
-url_service = URLService(
-    audio_service=audio_service,
-    video_service=video_service,
-    doc_service=document_service,
-)
+audio_service = AudioVoskTranscriptionService()
+video_service = VideoTranscriptionService()
+microsof_service = MicrosoftDocumnentToMdService()
+flat_text_service = FlatTextService()
+pdf_service = PDFTOMD4LLMService()
+services: List[BaseService] = [
+    audio_service,
+    video_service,
+    microsof_service,
+    flat_text_service,
+    pdf_service,
+]
+url_service = URLService(services=services)
 
 
 openai_settings = OpenAISettings()
@@ -51,33 +64,38 @@ def launch(self, task: str):
         )
 
         if isinstance(task.content, URLModel):
-            texts = url_service.transform_to_text(file_path=task.content.url)
+            task = url_service.process_task(task=task)
 
         elif isinstance(task.content, DocumentModel):
             file_data = file_connector.get_by_task_id(user_id=task.user_id, task_id=task.id)
             with open(task.content.raw_filename, "wb") as f:
                 f.write(file_data.read())
-            texts = document_service.transform_to_text(task.content.raw_filename, content_type=task.content.content_type)
+            task.content.file_path = task.content.raw_filename
+
+            for service in services:
+                if service.is_availble(task.content.content_type):
+                    task = service.process_task(task=task)
+
             if os.path.exists(task.content.raw_filename):
                 os.remove(task.content.raw_filename)
 
         elif isinstance(task.content, TextModel):
-            texts = [TextModelService(text=task.content.text, extras=task.content.model_dump())]
+            task.result = ResultModel(
+                type="flat",
+                created_at=task.content.created_at,
+                model_name="flat",
+                model_version=__version__,
+                percentage=1,
+                texts_found=[task.content.text],
+            )
 
         else:
             raise NotImplementedError("Content type not supported")
 
-        task.extras["texts"] = [text.model_dump() for text in texts]
-        task_table.update_task(
-            task_id=task.id,
-            form_data=TaskUpdateForm(
-                status=TaskStatus.IN_PROGRESS.value,
-                updated_at=int(time.time()),
-                extras=task.extras,
-            ),
+        splitted_text = split_texts_by_word_limit(
+            [text for text in task.result.texts_found],
+            max_words=int(model_context_length / 2),
         )
-
-        splitted_text = split_texts_by_word_limit([text.text for text in texts], max_words=int(model_context_length / 2))
 
         task, _ = merge_summaries(
             task=task,
