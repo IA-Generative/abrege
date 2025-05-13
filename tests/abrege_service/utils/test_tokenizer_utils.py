4272a79069202fb4ee642e7ee2c547e63140b654
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-13 16:58:20 +0200

    fix: add hf_model

--- tests/abrege_service/utils/test_tokenizer_utils.py
+++ tests/abrege_service/utils/test_tokenizer_utils.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from tiktoken import Encoding
 from abrege_service.utils.tokenizer import get_tokenizer_model
+import pytest
 
 
 def test_get_gpt_token():
@@ -19,3 +20,10 @@ def test_get_other_tokenizer():
     full_text = "I am angry"
     tokens = actual.encode(full_text)
     assert len(tokens) > 1
+
+
+@pytest.mark.skipif(os.environ.get("HF_TOKEN") is None, reason="requires HF_TOKEN to be sure we have access to the hf repo")
+def test_get_mistral_small_3_1_24B():
+    model_name = "mistralai/Mistral-Small-3.1-24B-Instruct-2503"
+    actual = get_tokenizer_model(model_name=model_name, cache_dir="tokenizer/")
+    assert actual is not None
