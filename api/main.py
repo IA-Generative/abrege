ab1cbb395b9a7f2d41786a0601c6450a73df3c4e
mitchou10 <helal.michou.junk@gmail.com>
2025-07-28 15:11:43 +0200

    fix: use correct path

--- api/main.py
+++ api/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(health_router)
+app.include_router(health_router, prefix="/api")
 app.include_router(summarize_router, prefix="/api")
 app.include_router(task_router, prefix="/api")
 app.include_router(doc_router, prefix="/api")
