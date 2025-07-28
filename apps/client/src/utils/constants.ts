a2c0bc58238522e131919664b27b9745beb98c00
David MENDY <davebull.dm@gmail.com>
2025-07-28 17:15:28 +0200

    feat: :chart_with_upwards_trend: add matomo tracking

--- apps/client/src/utils/constants.ts
+++ apps/client/src/utils/constants.ts
@@ -4,3 +4,7 @@ export const ABREGE_API_URL = (window as any).VITE_ABREGE_API_URL ?? import.meta
 // EXTERNAL LINKS
 export const USER_REVIEW_URL = (window as any).VITE_USER_REVIEW_URL ?? import.meta.env.VITE_USER_REVIEW_URL
 export const CHAP_CANAL_URL = (window as any).VITE_CHAP_CANAL_URL ?? import.meta.env.VITE_CHAP_CANAL_URL
+
+// MATOMO
+export const MATOMO_SITE_URL = (window as any).VITE_MATOMO_SITE_URL ?? import.meta.env.VITE_MATOMO_SITE_URL
+export const MATOMO_SITE_ID = (window as any).VITE_MATOMO_SITE_ID ?? import.meta.env.VITE_MATOMO_SITE_ID
