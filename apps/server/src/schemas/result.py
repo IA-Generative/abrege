408a813b719890c030c47b45b629f4ce543c9c7e
mitchou10 <helal.michou.junk@gmail.com>
2025-07-28 17:23:14 +0200

    refactor: move backend and frontent into folders to apps/server/ and apps/client

---
+++ apps/server/src/schemas/result.py
@@ -0,0 +1,33 @@
+from typing import Dict, Optional, Any, List, Union
+from pydantic import BaseModel, ConfigDict
+
+
+class Text(BaseModel):
+    id: str
+    text: str
+    word_count: int
+
+
+class PartialSummary(Text):
+    text1: Text
+    text2: Text
+
+
+class ResultModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    type: str
+    created_at: int
+    model_name: str
+    model_version: str
+    updated_at: Optional[int] = None
+    texts_found: Optional[List[str]] = []
+    percentage: float = 0.0
+    extras: Optional[Dict[str, Any]] = None
+    partial_summaries: Optional[List[Union[PartialSummary, Text]]] = []
+
+
+class SummaryModel(ResultModel):
+    summary: str
+    word_count: int
+    nb_llm_calls: Optional[int] = 0
+    type: str = "summary"
