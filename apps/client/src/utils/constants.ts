382f3a41f39e8d997c26d5275a9f007fc9b34da5
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-07-28 17:23:12 +0200

    fix: :bug: refactor links & add portal links

--- apps/client/src/utils/constants.ts
+++ apps/client/src/utils/constants.ts
@@ -4,3 +4,4 @@ export const ABREGE_API_URL = (window as any).VITE_ABREGE_API_URL ?? import.meta
 // EXTERNAL LINKS
 export const USER_REVIEW_URL = (window as any).VITE_USER_REVIEW_URL ?? import.meta.env.VITE_USER_REVIEW_URL
 export const CHAP_CANAL_URL = (window as any).VITE_CHAP_CANAL_URL ?? import.meta.env.VITE_CHAP_CANAL_URL
+export const PORTAIL_URL = (window as any).VITE_PORTAIL_URL ?? import.meta.env.VITE_PORTAIL_URL
