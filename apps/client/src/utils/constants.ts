3ac4326636858eaaa96d7a75f944b5d6c0f7b347
David MENDY <davebull.dm@gmail.com>
2025-07-29 12:46:48 +0200

    fix: :art: fix linting

--- apps/client/src/utils/constants.ts
+++ apps/client/src/utils/constants.ts
@@ -1,10 +1,9 @@
 // API
 export const ABREGE_API_URL = (window as any).VITE_ABREGE_API_URL ?? import.meta.env.VITE_ABREGE_API_URL
-
 // EXTERNAL LINKS
 export const USER_REVIEW_URL = (window as any).VITE_USER_REVIEW_URL ?? import.meta.env.VITE_USER_REVIEW_URL
-export const CHAP_CANAL_URL = (window as any).VITE_CHAP_CANAL_URL ?? import.meta.env.VITE_CHAP_CANAL_URL
-
+export const TCHAP_CANAL_URL = (window as any).VITE_TCHAP_CANAL_URL ?? import.meta.env.VITE_TCHAP_CANAL_URL
+export const PORTAIL_URL = (window as any).VITE_PORTAIL_URL ?? import.meta.env.VITE_PORTAIL_URL
 // MATOMO
 export const MATOMO_SITE_URL = (window as any).VITE_MATOMO_SITE_URL ?? import.meta.env.VITE_MATOMO_SITE_URL
 export const MATOMO_SITE_ID = (window as any).VITE_MATOMO_SITE_ID ?? import.meta.env.VITE_MATOMO_SITE_ID
