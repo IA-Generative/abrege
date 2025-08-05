a52a52d48733b903248f64e2f069ced828fff1a4
David MENDY <davebull.dm@gmail.com>
2025-08-05 11:48:58 +0200

    feat: :passport_control: add keycloak

--- apps/client/src/utils/constants.ts
+++ apps/client/src/utils/constants.ts
@@ -7,3 +7,9 @@ export const PORTAIL_URL = (window as any).VITE_PORTAIL_URL ?? import.meta.env.V
 // MATOMO
 export const MATOMO_SITE_URL = (window as any).VITE_MATOMO_SITE_URL ?? import.meta.env.VITE_MATOMO_SITE_URL
 export const MATOMO_SITE_ID = (window as any).VITE_MATOMO_SITE_ID ?? import.meta.env.VITE_MATOMO_SITE_ID
+
+// KEYCLOAK
+export const KEYCLOAK_URL = (window as any).VITE_KEYCLOAK_URL ?? import.meta.env.VITE_KEYCLOAK_URL
+export const KEYCLOAK_CLIENT_ID = (window as any).VITE_KEYCLOAK_CLIENT_ID ?? import.meta.env.VITE_KEYCLOAK_CLIENT_ID
+export const KEYCLOAK_REALM = (window as any).VITE_KEYCLOAK_REALM ?? import.meta.env.VITE_KEYCLOAK_REALM
+export const KEYCLOAK_REDIRECT_URI = (window as any).VITE_REDIRECT_URI ?? import.meta.env.VITE_REDIRECT_URI
