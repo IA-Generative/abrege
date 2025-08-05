2a9be9cd78781bf51983e8e9c4e9e13aab54d4f2
David MENDY <davebull.dm@gmail.com>
2025-08-05 11:48:58 +0200

    feat: :passport_control: add keycloak

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
+import { getKeycloak } from '@/utils/keycloak'
 import useToaster from './composables/use-toaster'
+
+const keycloak = getKeycloak()
+const isLoggedIn = ref<boolean | undefined>(keycloak.authenticated)
 
 const toaster = useToaster()
 
 const serviceTitle = 'Résumer un texte'
 const serviceDescription = 'Produire une synthèse de documents volumineux'
 const logoText = ['Ministère', 'de l’intérieur']
+
+const quickLinks = computed(() => {
+  const items = []
+  if (!isLoggedIn.value) {
+    items.push(
+      { label: 'Se connecter', to: '/login', class: 'fr-icon-user-fill' },
+      { label: 'S\'enregister', to: '/register', class: 'fr-icon-user-add-fill' }
+    )
+  }
+  else {
+    items.push(
+      { label: 'Se déconnecter', to: '/logout', class: 'fr-icon-logout-box-r-line' }
+    )
+  }
+
+  return items
+})
 </script>
 
 <template>
   <DsfrHeader
     :service-title="serviceTitle"
     :service-description="serviceDescription"
+    :quick-links="quickLinks"
     :logo-text="logoText"
   />
 
--- apps/client/src/router/index.ts
+++ apps/client/src/router/index.ts
@@ -1,10 +1,39 @@
+import type { RouteLocationNormalized } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 import ResumeView from '@/views/Index.vue'
 
 const routes = [
   {
     path: '/',
     name: 'resume',
+    component: ResumeView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    beforeEnter: async (_to: RouteLocationNormalized, _from: RouteLocationNormalized) => {
+      const userStore = useUserStore()
+      await userStore.login()
+    },
+    component: ResumeView,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    beforeEnter: async (_to: RouteLocationNormalized, _from: RouteLocationNormalized) => {
+      const userStore = useUserStore()
+      await userStore.register()
+    },
+    component: ResumeView,
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    beforeEnter: async (_to: RouteLocationNormalized, _from: RouteLocationNormalized) => {
+      const userStore = useUserStore()
+      await userStore.logout()
+    },
     component: ResumeView,
   },
 ]
