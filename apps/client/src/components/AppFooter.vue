3c7e1de942ad0f2e4ba7e6ccfaf784361bb223b2
Mitchou10 <helal.michou.junk@gmail.com>
2026-09-11 10:10:22 +0200

    feat: add AppFooter component and API routes documentation

--- apps/client/src/components/AppFooter.vue
+++ apps/client/src/components/AppFooter.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import useAppVersion from '@/composables/use-app-version'
+import { ABREGE_API_URL } from '@/utils/constants'
 
 const { frontendVersion, backendVersion, fetchBackendVersion } = useAppVersion()
 
 onMounted(fetchBackendVersion)
+
+const ecosystemLinks = [
+  {
+    label: 'Swagger API',
+    href: `${ABREGE_API_URL}/docs`,
+    title: 'Documentation interactive de l\'API (Swagger)',
+  },
+  {
+    label: 'Dépôt du projet',
+    href: 'https://github.com/IA-Generative/abrege',
+    title: 'Code source sur GitHub',
+  },
+]
 </script>
 
 <template>
   <DsfrFooter
+    :ecosystem-links="ecosystemLinks"
     :licence-text="`Frontend v${frontendVersion} - Backend v${backendVersion ?? '…'}`"
   />
 </template>
